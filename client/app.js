55fe1cd0237fd0bdf161887d054c5a50e600df38
Thompson Harris <william.thompson.harris@gmail.com>
2020-02-25 10:21:59 -0500

    basic redux

--- client/app.js
+++ client/app.js
@@ -1,11 +1,32 @@
 import React from 'react';
 //css
 import './app.css';
+// react-redux
+import { connect } from 'react-redux';
+//action
+import { setToggleSlideMenu } from './redux/nav/action/nav.action';
 
 class App extends React.Component {
+  componentDidMount() {
+    this.props.setToggleSlideMenu();
+  }
+
+  handleClick = e => {
+    e.preventDefault();
+    this.props.setToggleSlideMenu();
+  };
   render() {
-    return <div>React is working</div>;
+    return (
+      <div>
+        React is working
+        <button onClick={e => this.handleClick(e)}>test</button>
+      </div>
+    );
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setToggleSlideMenu: () => dispatch(setToggleSlideMenu()),
+});
+
+export default connect(null, mapDispatchToProps)(App);
