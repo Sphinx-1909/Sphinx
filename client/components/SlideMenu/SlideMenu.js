c597e22b0c72c829279e4c4d02ea1cb2e4905f61
Skipups <svetlana.rovinsky@gmail.com>
2020-02-26 20:18:59 -0500

    sd

--- client/components/SlideMenu/SlideMenu.js
+++ client/components/SlideMenu/SlideMenu.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './SlideMenu.css';
 //import { StyledMenu } from './Menu.styled';
 import { Link } from 'react-router-dom';
+import LogIn from '../login/login';
 class SlideMenu extends React.Component {
   render() {
     return (
@@ -17,6 +18,7 @@ class SlideMenu extends React.Component {
         <a>CHANNELS</a>
         <a>SETTINGS</a>
         <Link to="/account">Account</Link>
+        <Login />
       </div>
     );
   }
