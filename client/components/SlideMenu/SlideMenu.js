74e48d337c4cb0c996db0ae9a37effadcb8b2064
Skipups <svetlana.rovinsky@gmail.com>
2020-02-27 07:42:52 -0500

    in preogress

--- client/components/SlideMenu/SlideMenu.js
+++ client/components/SlideMenu/SlideMenu.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 //css
 import './SlideMenu.css';
 //import { StyledMenu } from './Menu.styled';
 import { Link } from 'react-router-dom';
 import LogIn from '../login/login';
+import { logOutAttempt } from '../../redux/authentication/authentication';
 class SlideMenu extends React.Component {
+  loginStatus = () => {
+    const { authentication } = this.props;
+    console.log(authentication);
+    // const { isLoggedIn } = authentication;
+  //   if (isLoggedIn) {
+  //     return (
+  //       <div>
+  //         <Link to="/account" style={{ textDecoration: 'none' }}>
+  //           Account
+  //         </Link>
+  //         <Button onClick={this.props.signout}>Log Out!</Button>
+  //       </div>
+  //     );
+  //   }
+  //   return (
+  //     <div>
+  //       <Link to="/login" style={{ textDecoration: 'none' }}>
+  //         <Button>Log in / Register</Button>
+  //       </Link>
+  //     </div>
+  //   );
+  // };
   render() {
     return (
       <div
@@ -17,11 +41,18 @@ class SlideMenu extends React.Component {
       >
         <a>CHANNELS</a>
         <a>SETTINGS</a>
-        <Link to="/account">Account</Link>
-        <Login />
+        {/*} {this.loginStatus()}*/}
       </div>
     );
   }
 }
 
-export default SlideMenu;
+const mapStateToProps = state => ({
+  authentication: state.authentication,
+});
+
+const mapDispatchToProps = dispatch => ({
+  signout: () => dispatch(logOutAttempt()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SlideMenu);
