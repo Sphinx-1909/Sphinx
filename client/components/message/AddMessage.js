704ec523ee76711afcd17e19664733e2d547e9ea
Lachlan Glen <lachlanjglen@gmail.com>
2020-02-28 18:34:23 -0500

    fix to wait for current position to load before component renders

--- client/components/message/AddMessage.js
+++ client/components/message/AddMessage.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { connect } from 'react-redux';
 import { addMessage } from '../../redux/messages';
 import '../../app.css'
+import { setLogInError } from '../../redux/authentication/authentication';
 
 const AddMessage = (props) => {
 
@@ -11,16 +12,21 @@ const AddMessage = (props) => {
   const [channelId, setChannelId] = useState('');
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
+  const [success, setSuccess] = useState(false)
+  const [error, setError] = useState(false)
+  const [positionLoaded, setPositionLoaded] = useState(false)
 
   const { channels } = props;
 
   navigator.geolocation.getCurrentPosition(pos => {
+    setPositionLoaded(true)
     setLatitude(pos.coords.latitude);
     setLongitude(pos.coords.longitude);
   })
 
   const handleSubmit = async ev => {
     ev.preventDefault();
+
     const postBody = {
       fileType: 'text',
       messageTitle: title,
@@ -29,25 +35,45 @@ const AddMessage = (props) => {
       longitude,
       radius: 1,
       channelId,
-    }
+    };
+    // console.log('postBody: ', postBody)
     await props.addMessage(postBody)
+    setSuccess(true)
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Channel</label>
-      <select onChange={ev => setChannelId(ev.target.value)}>
-        <option>{''}</option>
-        {
-          channels.map(channel => <option value={channel.id} idx={channel.id}>{channel.channelTitle}</option>)
-        }
-      </select>
-      <label>Title</label>
-      <input placeholder='Title' value={title} name="messageTitle" onChange={ev => setTitle(ev.target.value)} />
-      <label>Body</label>
-      <textarea placeholder='Message Body' value={body} name="messageBody" onChange={ev => setBody(ev.target.value)} />
-      <button type='submit'>POST</button>
-    </form>
+    success ? (
+      <div>
+        <h2>Success posting new message!</h2>
+        <a href='/'>Exit</a>
+      </div>
+    ) : (
+        <>
+          {
+            positionLoaded ? (
+              <form onSubmit={handleSubmit}>
+                <label>Channel</label>
+                <select onChange={ev => setChannelId(ev.target.value)}>
+                  <option>{''}</option>
+                  {
+                    channels.map(channel => <option value={channel.id} key={channel.id}>{channel.channelTitle}</option>)
+                  }
+                </select>
+                <label>Title</label>
+                <input placeholder='Title' value={title} name="messageTitle" onChange={ev => setTitle(ev.target.value)} />
+                <label>Body</label>
+                <textarea placeholder='Message Body' value={body} name="messageBody" onChange={ev => setBody(ev.target.value)} />
+                <button type='submit'>POST</button>
+                {
+                  error && <div style={{ backgroundColor: 'red' }}>Error posting new message. Please refresh the page and try again.</div>
+                }
+              </form>
+            ) : (
+                <h3>Loading...</h3>
+              )
+          }
+        </>
+      )
   );
 }
 
