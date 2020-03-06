1dcc5997f3c0dca865491d59f7a6470d6469a488
Lachlan Glen <lachlanjglen@gmail.com>
2020-03-06 17:50:17 -0500

    minor change

--- client/components/login/2FA/Form.js
+++ client/components/login/2FA/Form.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const AuthForm = () => {
   return (
     <form>
-      <div>Please enter your phone number to receive a verification code</div>
+      <div>Please enter your 10-digit phone number to receive a verification code</div>
       <input type='text'></input>
       <button>Get Verification Code</button>
     </form>
