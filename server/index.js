2d9d9765196a649b219c25e95f07090088c46b4c
lachlanglen <54282009+lachlanglen@users.noreply.github.com>
2020-04-05 09:10:53 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,16 +30,16 @@ app.use(cookieParser());
 app.use(bodyParser.json({ limit: '20mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:4000"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-app.use((req, res, next) => {
-  req.referer = 'http://localhost:4000/';
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+// app.use((req, res, next) => {
+//   req.referer = 'http://localhost:4000/';
+//   next();
+// });
 
 app.use((req, res, next) => {
   // console.log('req.cookies.sessionId in app.use: ', req.cookies.sessionId)
